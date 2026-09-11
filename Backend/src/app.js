5d0915b3724a6e9ef5b67b63c1e480e5f33798bf
rudhar-cmd <rudhargupta51@gmail.com>
2026-09-11 12:47:24 +0530

    Update CORS for production frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ import { redirectToOriginalUrl } from "./controller/url.controller.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://shortify11.netlify.app",
     credentials: true
 }));
 
